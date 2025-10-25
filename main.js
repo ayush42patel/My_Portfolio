3a04b11e5f4fb7a500f85c9d8bee13b461c01b42
ayush42patel <ayush42patel@gmail.com>
2025-10-25 23:36:29 +0530

    Badges

--- main.js
+++ main.js
@@ -219,3 +219,27 @@ window.onclick = function(event) {
     }
   });
 };
+// ======= Badge Carousel =======
+var badgeSwiper = new Swiper(".myBadgeSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1024: { slidesPerView: 3 },
+    768: { slidesPerView: 2 },
+    480: { slidesPerView: 1 },
+  },
+});
